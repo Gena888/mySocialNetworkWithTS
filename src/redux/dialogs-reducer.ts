120d01df7a7c5f7c633a8166c569f73ee519199a
Gena888 <theGena888@gmail.com>
2020-12-20 23:23:35 +0300

    dialogs reducer with ts

---
+++ src/redux/dialogs-reducer.ts
@@ -0,0 +1,72 @@
+import Richard from './../friendsIcons/recherd.png';
+import Erlich from './../friendsIcons/bahnam.png';
+import Gilfoyle from './../friendsIcons/gilfoyle.png';
+import Dinesh from './../friendsIcons/dinesh.png';
+import Message from './../components/Dialogs/Message/Message';
+
+const ADD_NEW_MESSAGE = 'ADD-NEW-MESSAGE';
+
+export type InilialStateType = typeof inilialState
+
+type dialogsDataType = {
+    id: number
+    imgSrc: string
+    name: string
+}
+
+type messagesDataType = {
+    id: number
+    message: string
+    addresserYou: boolean
+}
+
+let inilialState = {
+    dialogsData: [
+        { id: 1, imgSrc: 'https://sun1.beltelecom-by-minsk.userapi.com/c847220/v847220418/c6148/DDmHnFk6Uvw.jpg', name: 'Gena' },
+        { id: 2, imgSrc: Erlich, name: 'Erlich' },
+        { id: 3, imgSrc: Richard, name: 'Richard' },
+        { id: 4, imgSrc: Gilfoyle, name: 'Gilfoyle' },
+        { id: 5, imgSrc: Dinesh, name: 'Dinesh' }
+    ] as Array<dialogsDataType>,
+    messagesData: [
+        { id: 1, message: 'Hi, how\'s yours project?', addresserYou: true },
+        { id: 2, message: 'Hi, fine, but i cant finish messages page', addresserYou: false },
+        { id: 3, message: 'Why?', addresserYou: true },
+        { id: 4, message: 'i\'v got no API to interact with messages data', addresserYou: false },
+        { id: 5, message: 'Just create a static component with messages', addresserYou: true },
+        { id: 6, message: 'Ok bro, i\'ll make something like this, thank\'s you ', addresserYou: false },
+        { id: 7, message: 'You are welcome ', addresserYou: true }
+    ] as Array<messagesDataType>
+}
+
+const dialogsReducer = (state = inilialState, action: any) => {
+
+    switch (action.type) {
+
+        case ADD_NEW_MESSAGE:
+            let newMessage = action.newMessageBody;
+            return {
+                ...state,
+                messagesData: [...state.messagesData, {
+                    id: 6,
+                    message: newMessage,
+                    addresserYou: false
+                }],
+            };
+
+        default:
+            return state;
+    }
+}
+
+type addNewMessageACType = {
+    type: typeof ADD_NEW_MESSAGE,
+    newMessageBody: string
+}
+
+export const addNewMessageAC = (newMessageBody: string):addNewMessageACType => ({
+    type: ADD_NEW_MESSAGE,
+    newMessageBody
+});
+
+export default dialogsReducer;
