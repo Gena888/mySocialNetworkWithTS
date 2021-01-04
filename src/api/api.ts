624ab10515815c0c32457df7f0f94fc94016a5b9
Gena888 <theGena888@gmail.com>
2021-01-04 20:00:51 +0300

    api ts done.

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse } from "axios";
-import { LoginResponseType, MeResponseType } from "../types/apiTypes";
+import { GetUserType, PostPutDeleteRegularResponse, GetCaptureUrlType, LoginResponseType, MeResponseType, ProfileApiType, ProfilePhotoApiType } from "../types/apiTypes";
 import { ProfileType } from "../types/types";
 import Message from './../components/Dialogs/Message/Message';
 
@@ -10,55 +10,45 @@ const instanse = axios.create({
     headers: {
         'API-KEY': 'fd404da8-ea6d-494b-90d2-bb5c412e9027'
     }
-}
-);
-
+});
 
 
 export const userAPI = {
     unfollowUser(id: number) {
-        return instanse.delete(`follow/${id}`)
+        return instanse.delete<PostPutDeleteRegularResponse>(`follow/${id}`)
             .then(response => response.data)
     },
-
-
     followUser(id: number) {
-        return instanse.post(`follow/${id}`, {})
+        return instanse.post<PostPutDeleteRegularResponse>(`follow/${id}`, {})
             .then(response => response.data)
     },
-
     getUsers(currentPage: number, pageSize: number) {
-        return instanse.get(`users?page=${currentPage}&count=${pageSize} `)
+        return instanse.get<GetUserType>(`users?page=${currentPage}&count=${pageSize} `)
             .then(response => response.data)
     },
-
     getUsers2(pageNumber: number, pageSize: number) {
-        return instanse.get(`users?page=${pageNumber}&count=${pageSize}`)
+        return instanse.get<GetUserType>(`users?page=${pageNumber}&count=${pageSize}`)
             .then(response => response.data)
     }
 }
 
-
-
 export const profileAPI = {
     getProfileData(profileId: number) {
-        return instanse.get('profile/' + profileId)
+        return instanse.get<ProfileApiType>('profile/' + profileId)
             .then(response => response.data)
     },
-
     getStatus(userId: number) {
-        return instanse.get('profile/status/' + userId)
+        return instanse.get<string>('profile/status/' + userId)
             .then(response => response.data)
     },
-
     updateStatus(status: string) {
-        return instanse.put('profile/status/', { status: status })
+        return instanse.put<PostPutDeleteRegularResponse>('profile/status/', { status: status })
             .then(response => response.data)
     },
     putNewPhoto(photoFile: any) {
         let formData = new FormData();
         formData.append('image', photoFile)
-        return instanse.put('profile/photo', formData, {
+        return instanse.put<ProfilePhotoApiType>('profile/photo', formData, {
             headers: {
                 'Content-Type': 'mulipart/form-data'
             }
@@ -66,21 +56,17 @@ export const profileAPI = {
             .then(response => response.data)
     },
     saveProfile(profile: ProfileType) {
-        return instanse.put('profile', profile)
+        return instanse.put<PostPutDeleteRegularResponse>('profile', profile)
             .then(response => response.data)
     }
-
 }
-
 
 export const authAPI = {
 
     me() {
         return instanse.get<MeResponseType>('auth/me')
             .then(response => response.data)
-
     },
-
     Login(email: string, password: string, rememberMe: boolean = false, captcha: string | null = null) {
         return instanse.post<LoginResponseType>('auth/login', {
             email,
@@ -90,17 +76,15 @@ export const authAPI = {
         })
             .then(response => response.data)
     },
-
     Logout() {
-        return instanse.delete('auth/login')
+        return instanse.delete<PostPutDeleteRegularResponse>('auth/login')
             .then(response => response.data)
     }
-
 }
 
 export const securityAPI = {
     getCaptchaUrt() {
-        return instanse.get('security/get-captcha-url')
+        return instanse.get<GetCaptureUrlType>('security/get-captcha-url')
             .then(response => response.data)
     }
 }
--- src/types/apiTypes.ts
+++ src/types/apiTypes.ts
@@ -1,3 +1,34 @@
+
+// userAPI
+
+import { ContactsType, PhotosType } from "./types"
+
+export type GetUserType = {
+    count: number
+    page: number
+    term: string
+    friend: boolean
+}
+
+// profileAPI
+
+
+export type ProfileApiType = {
+    userId: number
+    lookingForAJob: boolean
+    lookingForAJobDescription: string
+    fullName: string
+    contacts: ContactsType
+    photos: PhotosType
+}
+
+export type ProfilePhotoApiType = {
+    data: PhotosType
+    resultCode: ResultCodesEnum
+    messages: Array<string>
+}
+
+// authAPI
 export type MeResponseType = {
     data: {
         id: number
@@ -15,6 +46,24 @@ export type LoginResponseType = {
         userId: number
     }
 }
+
+// securityAPI 
+
+export type GetCaptureUrlType = {
+    url: string
+}
+
+// common 
+
+export type PostPutDeleteRegularResponse = {
+    resultCode: ResultCodesEnum
+    messages: Array<string>
+    data: {
+        data: Array<string>
+    }
+}
+
+// enum's
 
 export enum ResultCodesEnum {
     Success = 0,
