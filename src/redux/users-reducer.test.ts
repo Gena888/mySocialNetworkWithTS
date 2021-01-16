f1a9d5d73064b59a79acaccca9916933a74efc2e
Gena888 <theGena888@gmail.com>
2021-01-16 20:41:10 +0300

    added user-reduser unit test

---
+++ src/redux/users-reducer.test.ts
@@ -0,0 +1,47 @@
+import { UsersType } from '../types/types';
+import usersReducer, { InitialStateType, UsersReducerActions } from './users-reducer'
+
+test('', () => {
+    const state: InitialStateType = {
+        usersData: [
+            {
+                id: 0,
+                name: ' 1st user',
+                followed: false,
+                photos: { small: undefined, large: undefined },
+                status: 'status 0'
+            },
+            {
+                id: 1,
+                name: ' 2st user',
+                followed: false,
+                photos: { small: undefined, large: undefined },
+                status: 'status 0'
+            },
+            {
+                id: 2,
+                name: ' 3st user',
+                followed: true,
+                photos: { small: undefined, large: undefined },
+                status: 'status 0'
+            },
+            {
+                id: 3,
+                name: ' 4st user',
+                followed: true,
+                photos: { small: undefined, large: undefined },
+                status: 'status 0'
+            }
+        ],
+        pageSize: 10,
+        totalUsersCount: 0,
+        currentPage: 1,
+        isFetching: false,
+        followingInProgress: []
+    };
+
+    const newState: InitialStateType = usersReducer(state, UsersReducerActions.follow(1))
+
+    expect(newState.usersData[0].followed).toBeFalsy();
+    expect(newState.usersData[1].followed).toBeTruthy();
+})
