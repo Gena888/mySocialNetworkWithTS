1d8459e0b2a45823854159c6f27093225ec572c9
Gena888 <theGena888@gmail.com>
2021-01-20 13:01:28 +0300

    created own hook 'usePreviouse'

---
+++ src/components/Hooks/usePreviouse.tsx
@@ -0,0 +1,10 @@
+import { useRef } from "react";
+import React, {useEffect} from 'react';
+
+export function usePrevious(value: any) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
